b23033634f5415f83e14b95390fa3ef4f579d110
davidelomundo02 <davidelomundo02@gmail.com>
2021-04-13 23:06:08 +0200

    Applicato PDO

--- includes/connection.php
+++ includes/connection.php
@@ -5,10 +5,24 @@ try {
     $dbname = "instantflow";
     $user = "root";
     $pass = "";
-    $db = new PDO ("mysql:host=$hostname;dbname=$dbname", $user, $pass);
+    $connessione = new PDO ("mysql:host=$hostname;dbname=$dbname", $user, $pass);
 } catch (PDOException $e) {
     echo "Errore: " . $e->getMessage();
     die();
 }
 
+/*
+    $dbHost="localhost";
+    $dbUserName="root";
+    $dbPasswordName="";
+    $dbName="instantflow";
+    
+    $connessione=mysqli_connect($dbHost, $dbUserName, $dbPasswordName, $dbName);
+    $database=mysqli_select_db($connessione, $dbName);
+    
+    // Check connection
+    if (!$connessione) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    */
 ?>
--- index.php
+++ index.php
@@ -1,10 +1,27 @@
 <?php
 require_once 'includes/navbar.php';
 require_once 'includes/header.php';
+require_once 'includes/connection.php';
 ?>
 
 
 <?php
-echo "My first PHP script!";
-//test
+
+$tmp = 'SELECT * FROM utenti;';
+
+$risultato=$connessione->query($tmp);
+$rows = $risultato->fetchAll(PDO::FETCH_NUM);
+
+foreach($rows as $row) {
+    //printf("$row[0] $row[1] $row[2] $row[3]\n");
+}
+
 ?>
+
+<div class="col-12 container">
+    <label for="email" class="form-label">Email</label>
+    <input type="email" class="form-control" id="email" placeholder="you@example.com">
+    <div class="invalid-feedback">
+    Please enter a valid email address for shipping updates.
+    </div>
+</div>
