f42f86bb72d0547d6194d0bd536b78afc3a6b3c7
davidelomundo02 <davidelomundo02@gmail.com>
2021-04-15 19:37:35 +0200

    fixed

--- includes/connection.php
+++ includes/connection.php
@@ -4,7 +4,7 @@ try {
     $hostname = "localhost";
     $dbname = "instantflow";
     $user = "root";
-    $pass = "instantPassword";
+    $pass = "";
     $connessione = new PDO ("mysql:host=$hostname;dbname=$dbname", $user, $pass);
 } catch (PDOException $e) {
     echo "Errore: " . $e->getMessage();
