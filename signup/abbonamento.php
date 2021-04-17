8dabca61329a79b567bf169f39aff1e1161b4137
davidelomundo02 <davidelomundo02@gmail.com>
2021-04-17 15:12:29 +0100

    Modifiche grafiche

--- signup/abbonamento.php
+++ signup/abbonamento.php
@@ -2,50 +2,52 @@
 require_once "../includes/header.php";
 ?>
 
-<div class="card-deck mb-3 text-center">
+<h1 class="text-center">Seleziona abbonamento</h1>
+
+<div class="row card-deck mb-3 text-center">
+      <div class="col">
         <div class="card mb-4 box-shadow">
           <div class="card-header">
-            <h4 class="my-0 font-weight-normal">Free</h4>
+            <h4 class="my-0 font-weight-normal">Basic</h4>
           </div>
           <div class="card-body">
-            <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1>
+            <h1 class="card-title pricing-card-title">€7,99 <small class="text-muted">/ mese</small></h1>
             <ul class="list-unstyled mt-3 mb-4">
-              <li>10 users included</li>
-              <li>2 GB of storage</li>
-              <li>Email support</li>
-              <li>Help center access</li>
+              <li>Risoluzione 720p</li>
             </ul>
             <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
           </div>
         </div>
+      </div>
+
+      <div class="col">
+        <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Plus</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">€9,99 <small class="text-muted">/ mese</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>Risoluzione 1080p</li>
+            </ul>
+            <button type="button" class="btn btn-lg btn-block btn-primary">Get started</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
         <div class="card mb-4 box-shadow">
           <div class="card-header">
             <h4 class="my-0 font-weight-normal">Pro</h4>
           </div>
           <div class="card-body">
-            <h1 class="card-title pricing-card-title">$15 <small class="text-muted">/ mo</small></h1>
+            <h1 class="card-title pricing-card-title">€14,99 <small class="text-muted">/ mese</small></h1>
             <ul class="list-unstyled mt-3 mb-4">
-              <li>20 users included</li>
-              <li>10 GB of storage</li>
-              <li>Priority email support</li>
-              <li>Help center access</li>
-            </ul>
-            <button type="button" class="btn btn-lg btn-block btn-primary">Get started</button>
-          </div>
-        </div>
-        <div class="card mb-4 box-shadow">
-          <div class="card-header">
-            <h4 class="my-0 font-weight-normal">Enterprise</h4>
-          </div>
-          <div class="card-body">
-            <h1 class="card-title pricing-card-title">$29 <small class="text-muted">/ mo</small></h1>
-            <ul class="list-unstyled mt-3 mb-4">
-              <li>30 users included</li>
-              <li>15 GB of storage</li>
-              <li>Phone and email support</li>
-              <li>Help center access</li>
+              <li>Risoluzione 2160p</li>
             </ul>
             <button type="button" class="btn btn-lg btn-block btn-primary">Contact us</button>
           </div>
         </div>
+      </div>
+
       </div>
