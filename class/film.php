85f4b856443463f22d1545a520987b8e31b45075
davidelomundo02 <davidelomundo02@gmail.com>
2021-04-25 17:24:51 +0100

    inizio get film

---
+++ class/film.php
@@ -0,0 +1,33 @@
+<?php
+class Film {
+
+  // connection
+  private $conn;
+
+  // table
+  private $db_table = "Films";
+
+  // Properties
+  public $id;
+  public $nome;
+  public $cognome;
+  public $numero;
+  public $scadenza;
+  public $cvv;
+  public $idUtente;
+
+  // db connection
+  public function __construct($db) {
+    $this->conn = $db;
+  }
+
+  // Methods
+  public function getFilms() {
+    $sqlQuery = "SELECT * FROM " . $this->db_table . ";";
+    $stmt = $this->conn->prepare($sqlQuery);
+
+    $stmt->execute();
+  }
+}
+
+?>
